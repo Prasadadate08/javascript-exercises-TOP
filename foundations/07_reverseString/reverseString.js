2484371eb6522b64cbe98eab90abfbd852597bab
prasadadate08 <128450165+Prasadadate08@users.noreply.github.com>
2026-09-09 13:08:41 +0530

    Add script for 07 reverseString exercise
    since str are immutable need a new variable to
    hold the reverseString.
    
    need to loop backward and add one letter a time
    and return it.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+// Need a str parameter in function to accept the argument that will be reversed
+// since str are immutable need a new variable where i can build reverse string
+// Need to loop through string one letter in reverse
+// Now add those letter to new variable and return
 
+const reverseString = function(str) {
+let reverseStr =''
+
+for (i = str.length - 1; i>=0; i--) {
+    reverseStr += str[i]
+}
+return reverseStr
 };
 
 // Do not edit below this line

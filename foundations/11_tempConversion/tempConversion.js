e1e94f3af773de65e5d21f9af6e5f79bde8022e4
prasadadate08 <128450165+Prasadadate08@users.noreply.github.com>
2026-09-09 19:03:57 +0530

    Add script for tempConversion exercise
    Looked for formula into wiki and used them to
    calculate accordingly.
    
    Looked for rounding method in js and used it.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+   let farenheit = (temp - 32) * 5/9
+   let oneDecimalFaren = Math.round(farenheit *10)/10
+  return oneDecimalFaren
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+   let celsius = (temp * 9/5 + 32)
+   let oneDecimalCelsius = Math.round(celsius *10)/10
+  return oneDecimalCelsius
 };
 
 // Do not edit below this line

b58a8b0decd53c8151f0c6c80113d9ca30ef9e72
prasadadate08 <128450165+Prasadadate08@users.noreply.github.com>
2026-09-09 17:10:48 +0530

    Add script for 09 sumAll exercise
    Took help from internet to swap variable so the
    range work properly from small to big.
    
    Plus to check if number is integer or not and
    learnt there is no direct method which check it.
    as isInteger consider -ve values true as well.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+// Need to pass 2 parameters
+// both should be positive
+// if parameter is anything else than positive number then give error
+// Need a variable to store the sum of numbers
+// sum the numbers between first to last parameter
 
+
+
+const sumAll = function(startNum,endNum) {
+
+    if (!Number.isInteger(startNum) || startNum < 0 || endNum < 0 || !Number.isInteger (endNum)) {
+        return 'ERROR';
+    }
+
+    if (startNum > endNum){
+       [startNum, endNum] = [endNum, startNum]
+    }
+
+    let sum = 0
+    for (i = startNum; i <=endNum; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

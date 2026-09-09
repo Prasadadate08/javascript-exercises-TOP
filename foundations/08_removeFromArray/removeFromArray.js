6f5b4c0837d6f85dfe0498f0c6c83783bec21591
prasadadate08 <128450165+Prasadadate08@users.noreply.github.com>
2026-09-09 15:41:57 +0530

    Add script to 08 removeFromArray exercise
    First wrote simple if condition to check that num
    exist in main array or not and skip it and
    added other.
    
    it only works for single argument and for multiple
    argument understand how to use them with rest
    parameters.
    
    Build the logic with includes and needed help from
    internet.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+// Need to pass array and a number and parameters in func
+// Need to check if the number which is second arg in present in array or not
+// For this need to loop  
+// check with if condition that value is present or not
+// if present either can delete it
+// or create a new array and push other values skipping this one
+
+const removeFromArray = function(array, ...theArgs) {
+let newArr = []
+let args = theArgs
+
+for (let num of array){
+    
+    if (args.includes(num)){
+        continue
+    }
+    else{
+        newArr.push(num)
+    }
+}
+
+return newArr
 };
 
 // Do not edit below this line

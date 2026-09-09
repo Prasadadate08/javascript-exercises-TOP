664613652c26e86176ea68ebb454efd10cb0fa1d
prasadadate08 <128450165+Prasadadate08@users.noreply.github.com>
2026-09-09 18:15:07 +0530

    Add script for 10 leapYears exercise
    Understood the condition and correctly placed it
    into the program and returned true or false.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+// Need a year parameter to check if it is leap year
+// if a year is divisible by 4 it is leap year
+// if year is divisible by 100 then it is not a leap year
+// if it is divisible by 400 then it is leap year
+// leap is both year % 4 && (!year % 100 or year % 400)
 
+const leapYears = function(year) {
+    if (year % 4 === 0 && (year % 100 !==0 || year % 400 === 0)) {
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
